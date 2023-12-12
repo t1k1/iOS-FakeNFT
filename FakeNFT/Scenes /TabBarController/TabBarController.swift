2e0425ccdd3b8de9d95e752deaafc5a0fa1246b3
Sergey Kemenov <skemenov@icloud.com>
2023-12-13 00:10:18 +0300

    added initial StatisticsVC as a navigation's root VC. Added dummies for other epics

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,30 +1,58 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    
+
     let servicesAssembly = ServicesAssembly(
         networkClient: DefaultNetworkClient(),
         nftStorage: NftStorageImpl()
     )
-    
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
+
+    private let profileTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.profile", comment: ""),
+        image: Statistics.SfSymbols.iconProfile,
         tag: 0
     )
-    
+    private let catalogTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.catalog", comment: ""),
+        image: Statistics.SfSymbols.iconCatalog,
+        tag: 1
+    )
+    private let cartTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.cart", comment: ""),
+        image: Statistics.SfSymbols.iconCart,
+        tag: 2
+    )
+    private let statisticsTabBarItem = UITabBarItem(
+        title: Statistics.Labels.tabBarStatistics,
+        image: Statistics.SfSymbols.iconStatistics,
+        tag: 3
+    )
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
-        
+        // mock data
+        let profileController = TestCatalogViewController(servicesAssembly: servicesAssembly)
+        let catalogController = TestCatalogViewController(servicesAssembly: servicesAssembly)
+        let cartController = TestCatalogViewController(servicesAssembly: servicesAssembly)
+
+        // my epic
+        let statisticsController = StatisticsViewController(servicesAssembly: servicesAssembly)
+        let navStatisticsController = UINavigationController(rootViewController: statisticsController)
+
+        profileController.tabBarItem = profileTabBarItem
         catalogController.tabBarItem = catalogTabBarItem
-        
-        viewControllers = [catalogController]
-        selectedIndex = 0
-        
+        cartController.tabBarItem = cartTabBarItem
+        statisticsController.tabBarItem = statisticsTabBarItem
+
+        viewControllers = [
+//            profileController,
+//            catalogController,
+//            cartController,
+            navStatisticsController
+        ]
+        selectedIndex = 3
+        tabBar.isTranslucent = false
+        tabBar.unselectedItemTintColor = .ypBlackDay
         view.backgroundColor = .systemBackground
     }
 }
