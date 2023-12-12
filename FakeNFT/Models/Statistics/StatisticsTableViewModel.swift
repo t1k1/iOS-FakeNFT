5a8491e5a924813a3be75c85d78eaba45b96f26c
Sergey Kemenov <skemenov@icloud.com>
2023-12-13 00:08:05 +0300

    Made structures `UserDetails`, `CollectionNFT` as view models for Statistics VCs

---
+++ FakeNFT/Models/Statistics/StatisticsTableViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  StatisticsTableViewModel.swift
+//  FakeNFT
+//
+//  Created by Sergey Kemenov on 10.12.2023.
+//
+
+import UIKit
+
+struct UserDetails: Hashable {
+    let urlPhoto: String
+    let name: String
+    let rating: Int
+    let description: String
+    let urlSite: String
+}
+
+struct CollectionNFT {
+    let image: UIImage
+    let isLiked: Bool
+    let rating: Int
+    let name: String
+    let price: Double
+    let currencyCode: String
+
+}
