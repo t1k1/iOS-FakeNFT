c07f105485dcdda8b26e6e6810065a8052c1aee2
IuriiKarpunov <karpunov.yury@yandex.ru>
2023-12-13 14:51:26 +0400

    Добавлена CollectionsModel

---
+++ FakeNFT/Models/CollectionsModel.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionsModel.swift
+//  FakeNFT
+//
+//  Created by Iurii on 13.12.23.
+//
+
+import Foundation
+
+public struct CollectionsModel {
+    let createdAt: Date
+    let name: String
+    let cover: String
+    let nfts: [String]
+    let description: String
+    let author:String
+    let id: String
+}
