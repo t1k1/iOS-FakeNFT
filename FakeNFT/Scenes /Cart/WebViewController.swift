9d1421be90a5b84b818d668ce76c72ba4c7c846c
ognerub <a.ulianovskiy@yandex.ru>
2023-12-16 18:22:37 +0300

    progressHUD добавлен на время загрузки Пользовательского соглашения

--- FakeNFT/Scenes /Cart/WebViewController.swift
+++ FakeNFT/Scenes /Cart/WebViewController.swift
@@ -1,35 +1,35 @@
 import UIKit
 import WebKit
+import ProgressHUD
 
 final class WebViewController: UIViewController {
     
     private lazy var webView = WKWebView(frame: CGRectMake(0, 0, view.bounds.width, view.bounds.height))
+    
+    private var estimatedProgressObservation: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = false
         if let url = URL(string: "https://yandex.ru/legal/practicum_termsofuse/") {
             webView.load(URLRequest(url: url))
-            webView.navigationDelegate = self
             view.addSubview(webView)
         }
-    }
-}
-
-extension WebViewController: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        print("error")
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("finish")
+        
+        estimatedProgressObservation = webView.observe(
+            \.estimatedProgress,
+             options: [],
+             changeHandler: {[weak self] _, _ in
+                 guard let self = self else { return }
+                 self.didUpdateProgressValue(self.webView.estimatedProgress)
+             })
     }
     
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("start")
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        print("error 2")
+    private func didUpdateProgressValue(_ newValue: Double) {
+        if newValue < 1.0 {
+            ProgressHUD.show()
+        } else {
+            ProgressHUD.dismiss()
+        }
     }
 }
